57c470dcec764d4bce826ab7a4caa1e7e6b13033
金 昌圭 <kim_ck@commerce21.co.jp>
2021-06-30 19:16:29 +0900

    DOM利用してデータ取得

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("login.js!");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", () => {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(`id:${req.id}, psword:${req.psword}`);
+});
